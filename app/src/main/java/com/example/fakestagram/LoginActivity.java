98a6a1c99134bbf8248e8f4e8818556ca29d0b5d
artballesteros <58927442+artballesteros@users.noreply.github.com>
2021-10-22 23:15:30 -0700

    add Login Activity

---
+++ app/src/main/java/com/example/fakestagram/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.fakestagram;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressWarnings("FieldCanBeLocal")
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
