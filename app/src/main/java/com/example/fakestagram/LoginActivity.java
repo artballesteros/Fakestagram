afa2702e23b24803834e54d895a4b9ef3674daa7
artballesteros <58927442+artballesteros@users.noreply.github.com>
2021-10-24 19:18:58 -0700

    fix login activity return bug

--- app/src/main/java/com/example/fakestagram/LoginActivity.java
+++ app/src/main/java/com/example/fakestagram/LoginActivity.java
@@ -29,6 +29,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
@@ -64,5 +68,6 @@ public class LoginActivity extends AppCompatActivity {
     private void goMainActivity() {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+        finish();
     }
 }
