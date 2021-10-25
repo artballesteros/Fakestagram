22861e65c9dc3977e159c043da714884df1552d3
artballesteros <58927442+artballesteros@users.noreply.github.com>
2021-10-24 19:47:57 -0700

    add post class and register it as parseObject subclass in ParseApplication.java

--- app/src/main/java/com/example/fakestagram/ParseApplication.java
+++ app/src/main/java/com/example/fakestagram/ParseApplication.java
@@ -3,12 +3,15 @@ package com.example.fakestagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("TZoAhieoOZ3a07sg1ddjVicAvwnT9ATwDveNKCZS")
                 .clientKey("yLkESLigeSn2fLjEnvZobPU9dBWMjGJSB9E009Z0")
