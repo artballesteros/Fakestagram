47ec1da7ca766d5260f258e70c70b4a72230257d
artballesteros <58927442+artballesteros@users.noreply.github.com>
2021-10-29 16:53:26 -0700

    create post fragment

--- app/src/main/java/com/example/fakestagram/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/fakestagram/fragments/ComposeFragment.java
@@ -171,20 +171,4 @@ public class ComposeFragment extends Fragment {
         });
     }
 
-    private void queryPost() {
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        query.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> posts, ParseException e) {
-                if (e != null) {
-                    Log.e(TAG, "Issue with getting posts", e);
-                    return;
-                }
-
-                for (Post post : posts) {
-                    Log.i(TAG, "Post: " + post.getDescription());
-                }
-            }
-        });
-    }
 }
--- app/src/main/java/com/example/fakestagram/fragments/PostsFragment.java
+++ app/src/main/java/com/example/fakestagram/fragments/PostsFragment.java
@@ -5,17 +5,33 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.fakestagram.Post;
+import com.example.fakestagram.PostsAdapter;
 import com.example.fakestagram.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PostsFragment extends Fragment {
+
+    public static final String TAG = "PostsFragment";
+    private RecyclerView rvPosts;
+    private PostsAdapter adapter;
+    private List<Post> allPosts;
 
     public PostsFragment() {
         // Required empty public constructor
@@ -31,5 +47,29 @@ public class PostsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        allPosts = new ArrayList<>();
+        rvPosts = view.findViewById(R.id.rvPosts);
+        adapter = new PostsAdapter(getContext(), allPosts);
+        rvPosts.setAdapter(adapter);
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPost();
+    }
+
+    private void queryPost() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
